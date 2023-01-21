9c6e917c7c764a086ad78f660aacbb1c828628b0
Stonetr03 <mrstonetr03@gmail.com>
2023-01-20 22:35:00 -0800

    README

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -1,10 +1,4 @@
 local RunService = game:GetService("RunService")
 
 local Debug = require(game.ReplicatedStorage.debug)
-Debug:addLine("Test","Test2")
-
-local TimeLine = Debug:addLine("Time","")
-
-RunService.RenderStepped:Connect(function(deltaTime)
-    TimeLine:Update(os.date("%c",os.time()))
-end)
+Debug:addLine("Example","Exampe Line")
--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,10 +1,4 @@
 local RunService = game:GetService("RunService")
 
 local Debug = require(game.ReplicatedStorage.debug)
-Debug:addLine("ServerTest","yes")
-
-local TimeLine = Debug:addLine("ServerTime","")
-
-RunService.Heartbeat:Connect(function(deltaTime)
-    TimeLine:Update(os.date("%c",os.time()))
-end)
+Debug:addLine("Example from Server","Value")
